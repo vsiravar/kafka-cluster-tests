c3f90bfd160578491bb9b00623de2213e5553809
Vishwas Siravara <vsiravara@gmail.com>
2024-03-13 21:47:07 -0700

    add tests

---
+++ certificate_explorer/src/test/java/com/vsiravara/kafka/KafkaBrokerCertificateExplorerTest.java
@@ -0,0 +1,21 @@
+package com.vsiravara.kafka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class KafkaBrokerCertificateExplorerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        KafkaBrokerCertificateExplorer.xmain();
+        assertTrue( true );
+    }
+}
